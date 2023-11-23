eaca813200d624afa5edb6c168a86bfb1b88a23e
AliHMohammad <alihmohammad@hotmail.com>
2023-11-23 13:00:09 +0100

    added comments

--- src/Subtask/SubtaskService.ts
+++ src/Subtask/SubtaskService.ts
@@ -11,19 +11,25 @@ export default class SubtaskService {
     public async updateSubtaskStatus(subtaskId: number, taskInstanceId: number) {
         const subtaskRepository = new SubtaskRepository();
         const taskRepository = new TaskRepository()
-        
+        //! Instancer et orderRepository til det sidste prisma kald i den her metode.
 
+        //Update the subtaskInstance and taskInstance if necesseray in this transaction
         await subtaskRepository.completeSubtask(subtaskId, taskInstanceId);
 
+        //Get the orderId
         const orderId = await taskRepository.getOrderIdByTaskInstanceId(taskInstanceId);
+
+        //Use the orderId to get all taskInstances in the order
         const taskInstancesInOrder = await taskRepository.getTaskInstancesInSingleOrder(orderId);
         
+        //Guard. Loop through taskInstances. If there still exists a taskInstance with status != COMPLETED, then exit the method and return to controller. 
         for (const taskInstance of taskInstancesInOrder) {
-            if (taskInstance.status === Status.IN_PROGRESS || taskInstance.status === Status.PENDING) {
+            if (taskInstance.status !== Status.COMPLETED) {
                 return;
             }
         }
 
+        //If you reach here, then all taskInstances are COMPLETED. We update the status on the order itself to COMPLETED
         //! SKAL KALDE PÅ EN METODE PATCH STATUS ORDER INDE I ORDER REPOSITORY!!
         return prisma.order.update({
             where: {
