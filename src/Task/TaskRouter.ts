31087cc235c35606b55ee3e93ed7993f151474ac
Chris Skov Sørensen <chso0002@stud.kea.dk>
2023-11-24 23:36:00 +0100

    added validation to routes

--- src/Task/TaskRouter.ts
+++ src/Task/TaskRouter.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import TaskController from "./TaskController.js";
+import {validateCreateComment, validateInitiateTask} from "../Utility/validation/taskValidation.js";
+import {validateIdParamsInt} from "../Utility/validation/validateIdParams.js";
 
 export const taskRouter = express.Router();
 const taskController = new TaskController();
 
-taskRouter.patch("/tasks/:id", taskController.initiateTaskExecutor);
-taskRouter.post("/tasks/:id/comments", taskController.createCommentExecutor)
+taskRouter.patch("/tasks/:id", validateInitiateTask, taskController.initiateTaskExecutor);
+taskRouter.post("/tasks/:id/comments", validateCreateComment, taskController.createCommentExecutor)
 taskRouter.get("/tasks", taskController.getTasksExecutor)
-taskRouter.get("/tasks/:id", taskController.getSingleTaskExecutor)
+taskRouter.get("/tasks/:id", validateIdParamsInt, taskController.getSingleTaskExecutor)
