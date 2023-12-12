cd14d85307f366485b1dc7f005ea4c3803d2eb93
AliHMohammad <alihmohammad@hotmail.com>
2023-12-12 12:14:08 +0100

    Removed unnecessary await

--- src/Customer/CustomerRepository.ts
+++ src/Customer/CustomerRepository.ts
@@ -110,7 +110,7 @@ export default class CustomerRepository {
     }
 
     public async createCustomer({ firstName, lastName, address, city, email, phone, zip, id }: CustomerReqBody) {
-        return await prisma.customer.create({
+        return prisma.customer.create({
             data: {
                 id,
                 firstName,
